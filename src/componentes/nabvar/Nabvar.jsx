60da7661da0e0e9162ac8b9d536093aae47cce8b
XavierGuerrero1989 <xavierignacioguerrero@gmail.com>
2022-08-05 10:25:27 -0300

    2.7 con logo en navbar

--- src/componentes/nabvar/Nabvar.jsx
+++ src/componentes/nabvar/Nabvar.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import BotonCarrito from '../botonCarrito/BotonCarrito';
 import BotonLogin from '../botonLogin/BotonLogIn';
+import logoimg from '../../imgs/logo.png'
 
 
 
@@ -11,12 +12,12 @@ const Nabvar = () => {
   return (
 
     <div className='container-fluid'>
-          <div className="row navbar justify-content-center">
-            <div className="col-md-2 text-center navbar__item">
-              < BotonLogin />
+          <div className="row navbar">
+          <div className="col-md-2 justify-self-start">
+                <Link to={'/'}><img src={logoimg} alt="logo" className='navbar__logo' /></Link>
             </div>
             <div className="col-md-2 text-center navbar__item">
-              <Link to={'/'} className="navbar__link">Home</Link>
+              < BotonLogin />
             </div>
             <div className="col-md-2 text-center navbar__item">
               <Link to='/categoria/dc' className="navbar__link">DC Comics</Link>
@@ -27,7 +28,7 @@ const Nabvar = () => {
             <div className="col-md-2 text-center navbar__item">
             <Link to='/sobreNosotros' className="navbar__link">Sobre Nosotros</Link>
             </div>
-            <div className="col-md-2 text-center navbar__item">
+            <div className="col-md-1 text-center navbar__item">
             < BotonCarrito />
             </div>
           </div>
